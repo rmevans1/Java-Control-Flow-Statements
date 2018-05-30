64fa9c8acfdf2cf70370ad400f7f731b06e23bc0
Robert Evans <robertevans@robertevansmb.com>
2018-05-30 14:09:02 -0400

    complete challenge

--- src/Lecture40.java
+++ src/Lecture40.java
@@ -29,5 +29,39 @@ public class Lecture40 {
                 System.out.println("Was not 1 or 2");
 
         }
+
+        //Challenge
+        // create a new char variable
+        // create a switch statement testing for
+        // A, B, C, D, or E
+        // display a message if any of these are found and then break
+        // Add a default which displays a message saying not found
+        char myChar = 'B';
+
+        switch (myChar){
+            case 'A':
+                System.out.println("A found");
+                break;
+
+            case 'B':
+                System.out.println("B found");
+                break;
+
+            case 'C':
+                System.out.println("C found");
+                break;
+
+            case 'D':
+                System.out.println("D found");
+                break;
+
+            case 'E':
+                System.out.println("E found");
+                break;
+
+            default:
+                System.out.println("Not found");
+                break;
+        }
     }
 }
