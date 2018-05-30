e743c886019a8b96f5f21bf25f1c889c0a0f80a4
Robert Evans <robertevans@robertevansmb.com>
2018-05-30 14:02:23 -0400

    Compare if and switch blocks

--- src/Lecture40.java
+++ src/Lecture40.java
@@ -1,5 +1,33 @@
 public class Lecture40 {
     public static void main(String[] args){
+        int value = 3;
+        if(value == 1) {
+            System.out.println("Value was 1");
+        } else if(value == 2){
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Was not 1 or 2");
+        }
 
+        int switchValue = 4;
+
+        switch (switchValue){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3: case 4: case 5:
+                System.out.println("Was a 3, or a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+
+        }
     }
 }
